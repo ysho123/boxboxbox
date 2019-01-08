ef7f321b13c603ea7b43e1ad7bbaa00bb3140178
ysho123 <569513303@qq.com>
2019-01-08 09:49:34 +0800

    1
    1

---
+++ box20-小小排/AldUtils/ald-stat-conf.js
@@ -0,0 +1,4 @@
+exports.app_key = "fc916c5fd8658b470811a36f5c8cc200"; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = false; //默认不获取用户坐标位置
+exports.plugin = false;  //您的小程序中是否使用了插件。根据是否启用插件会有不同的接入方式，请参考文档文档。
+
